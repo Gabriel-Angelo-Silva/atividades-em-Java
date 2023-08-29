d54479636f6f705695a598035f5beb5e12d37cf5
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2023-08-29 12:50:42 -0400

    Update Papel.java

--- Papel.java
+++ Papel.java
@@ -1,1 +1,9 @@
+class Papel {
+    String marca;
+    int papelU, paginasV;
 
+    public int preencher_Papel(int papelU, int paginasV) {
+        paginasV -= papelU;
+        return paginasV;
+    }
+}
